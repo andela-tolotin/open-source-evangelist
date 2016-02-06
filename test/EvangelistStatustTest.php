94574c7c7fb1a09993202fe6fe610ba5b83aada5
andela-tolotin <temitope.olotin@andela.com>
2016-02-06 14:21:53 +0100

    added comments to methods and variables

--- test/EvangelistStatustTest.php
+++ test/EvangelistStatustTest.php
@@ -7,7 +7,7 @@ namespace Laztopaz\OpenSourceEvangelistStatus\Test;
  * Run test on EvangelistStatusTest of user
  * 
  * @package  Laztopaz\OpenSourceEvangelistStatus\Test
- * @author   Temitope Olotin] <temitope.olotin@andela.com>
+ * @author   Temitope Olotin <temitope.olotin@andela.com>
  * @license  <https://opensource.org/license/MIT> MIT
  */
 
