86308edb4d246da756cb47feab3887295c63e0d5
andela-tolotin <temitope.olotin@andela.com>
2016-02-08 11:38:45 +0100

    Removed unneccessary variable declaration

--- src/Ranking/EvangelistStatusRanking.php
+++ src/Ranking/EvangelistStatusRanking.php
@@ -15,42 +15,24 @@ namespace Laztopaz\OpenSourceEvangelistStatus;
 
  class EvangelistStatusRanking implements EvangelistStatusRankingInterface
  {
- 	private $evangelistTypeMessage;
-
-    /**
-     *
-     * This is a constructor that accepts no of user repo on github
-     */
-    public function __construct()
-    {
-        
-    }
 
     public function determineEvangelistLevel($noOfRepo)
     {
-        if($noOfRepo == "")
-        {
+        if ($noOfRepo == "") {
             return false;
         }
 
-    	if($noOfRepo >= 5 && $noOfRepo<= 10){
-
-    		$this->evangelistTypeMessage = "Damn It!!! Please make the world better, Oh Ye Prodigal Junior Evangelist";
-
-    	} else if($noOfRepo >= 11 && $noOfRepo <= 20){
-
-    		$this->evangelistTypeMessage = "Keep Up The Good Work, I crown you Associate Evangelist";
-
-    	} else if($noOfRepo >= 21){
-
-    		$this->evangelistTypeMessage = "Yeah, I crown you Senior Evangelist. Thanks for making the world a better place";
-
-    	}else{
-
-            $this->evangelistTypeMessage = "Fuck Off!!! Please make the world better, Oh Ye Lazy Evangelist";
+    	if ($noOfRepo >= 5 && $noOfRepo<= 10) {
+    		$evangelistTypeMessage = "Damn It!!! Please make the world better, Oh Ye Prodigal Junior Evangelist";
+    	} else if ($noOfRepo >= 11 && $noOfRepo <= 20) {
+    		$evangelistTypeMessage = "Keep Up The Good Work, I crown you Associate Evangelist";
+    	} else if ($noOfRepo >= 21) {
+    		$evangelistTypeMessage = "Yeah, I crown you Senior Evangelist. Thanks for making the world a better place";
+    	} else {
+            $evangelistTypeMessage = "Fuck Off!!! Please make the world better, Oh Ye Lazy Evangelist";
         }
 
-    	return $this->evangelistTypeMessage;
+    	return $evangelistTypeMessage;
     }
 
  }
