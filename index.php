e0a7a9786a7196a19de45ad6a5303e498bc23c0a
andela-tolotin <temitope.olotin@andela.com>
2016-02-08 12:44:08 +0100

    save

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-
 namespace Laztopaz\OpenSourceEvangelistStatus;
-
 require 'vendor/autoload.php';
+use Laztopaz\OpenSourceEvangelistStatus\EvangelistStatusException;
+use Exception;
 
-$evan = new EvangelistStatus('andela-tolotin');
-echo $evan->getStatus();
+
+ //try {
+ 	//$username = new EvangelistStatusException();
+ 	//$username->checkEmptyGithubUsername('');
+ 	$evan = new EvangelistStatus('andela-tolotin');
+    echo $evan->getStatus();
+ //} catch (Exception $e) {
+ 	//echo 'Exception Caught: '.$e->getMessage();
+ //}
