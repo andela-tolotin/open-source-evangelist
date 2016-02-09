f7d0064a2b54f51dbc04bddda2590b9ab4d7c061
andela-tolotin <temitope.olotin@andela.com>
2016-02-09 16:51:15 +0100

    Added argument check  exception class

---
+++ src/Exceptions/ArgumentCheckException.php
@@ -0,0 +1,26 @@
+<?php
+namespace Laztopaz\OpenSourceEvangelistStatus;
+
+/**
+ *
+ * EvangelistStatus class
+ * 
+ * @package  Laztopaz\OpenSourceEvangelistStatus
+ * @author   Temitope Olotin <temitope.olotin@andela.com>
+ * @license  <https://opensource.org/license/MIT> MIT
+ */
+
+use Exception;
+
+class ArgumentCheckException extends Exception
+{
+    /**
+	 * This method return an instance of an exception
+	 * @param $exception
+	 * @return message
+	 */
+	public static function NullOfOverflowArgumentException($exception)
+	{
+		return new static($exception);
+	}
+}
