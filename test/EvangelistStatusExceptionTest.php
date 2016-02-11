3d0d42b3d092b6e479342b16aeb2ae7fd3f12e56
andela-tolotin <temitope.olotin@andela.com>
2016-02-11 11:04:18 +0100

    Add a test Exception class

---
+++ test/EvangelistStatusExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Laztopaz\OpenSourceEvangelistStatus\Test;
+
+/**
+ *
+ * Run test on EvangelistStatusTest of user
+ * 
+ * @package  Laztopaz\OpenSourceEvangelistStatus\Test
+ * @author   Temitope Olotin <temitope.olotin@andela.com>
+ * @license  <https://opensource.org/license/MIT> MIT
+ */
+
+
+use PHPUnit_Framework_TestCase;
+use Laztopaz\OpenSourceEvangelistStatus\EvangelistStatus;
+
+class EvangelistStatusExceptionTest extends PHPUnit_Framework_TestCase
+{	
+	/**
+	 * Class Instantiation of the EvangelistStatus class
+	 */
+    public function setUp()
+    {
+    	
+    }
+
+    /**
+     * @expectedException Laztopaz\OpenSourceEvangelistStatus\EvangelistStatusException
+     */
+    public function testGitHubUsernameEmptyException()
+    {
+        $evangelist = new EvangelistStatus('');
+        
+    }
+    
+}
